239edae168a6d964cdd425575ce7943a4eabdb17
Victor Le Roy <victorlrmatos@gmail.com>
2026-03-03 19:53:47 -0300

    feat: Mark Token and UserId properties as required in RefreshToken entity

--- backend/src/AuctionHouse.Domain/Entities/RefreshToken.cs
+++ backend/src/AuctionHouse.Domain/Entities/RefreshToken.cs
@@ -5,8 +5,8 @@ namespace AuctionHouse.Domain.Entities
     public class RefreshToken
     {
         public int Id { get; set; }
-        public string Token { get; set; }
-        public string UserId { get; set; }
+        public required string Token { get; set; }
+        public required string UserId { get; set; }
         public DateTime Expires { get; set; }
         public bool IsExpired => DateTime.UtcNow >= Expires;
         public DateTime Created { get; set; }
